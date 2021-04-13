4e1249b88490401b762c4e54737e9a8e05eda7b0
John Padi <johnpadi7@gmail.com>
2021-04-13 13:00:30 +0000

    number 7,15

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './signin-and-signup.styles.scss';
 import SignIn from '../../components/signin/signin.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
-	<div className='signin-and-signup'><SignIn/></div>
+	<div className='signin-and-signup'>
+		<SignIn/>
+		<SignUp/>
+	</div>
 );
 
 export default SignInAndSignUpPage;
