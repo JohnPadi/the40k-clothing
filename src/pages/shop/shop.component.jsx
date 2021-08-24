3f4214249254004493c62d498261ce584922a15b
John Padi <johnpadi7@gmail.com>
2021-08-24 15:55:14 +0000

    number 9,2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,42 @@
+/*
 import React from 'react';
 import './shop.styles.scss';
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+
+const ShopPage = ({collections}) =>{
+	<div className='shop-page'>
+		<CollectionsOverview/>	
+	</div>
+};
+
+export default ShopPage;
+*/
+
+import React from 'react';
+
+import SHOP_DATA from '../../redux/shop/shop.data.js';
+
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			collections: SHOP_DATA
-		};
-	}
-	render(){
-		const {collections} = this.state;
-		return (
-			<div className='shop-page'>
-				{
-					collections.map(({id,...otherCollectionProps})=>(
-						<CollectionPreview key={id}{...otherCollectionProps}/>
-					))
-				}
-			</div>
-		);
-	};
-};
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
